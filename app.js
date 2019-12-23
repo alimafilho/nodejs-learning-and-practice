d8eb2808f0225a269aa54db139bba9e2c9725988
antoniolima <antonio.filho@tvglobo.com.br>
2019-12-23 20:46:32 -0300

    implementação do ejs

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
   res.send("<html><body>Portal de noticias</body></html>")
 });
 
 app.get('/tecnologia', function(req, res){
-  res.send("<html><body>Portal de tecnologia</body></html>")
+  res.render("secao/tecnologia")
 });
 
 app.listen(3000, function () { console.log("Servidor rodando com express") })
