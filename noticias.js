81722bf0e045c83fadbe9e76ef0c345217fe85bb
antoniolima <antonio.filho@tvglobo.com.br>
2019-12-23 20:22:20 -0300

    Definindo rotas das requisições http

--- noticias.js
+++ noticias.js
@@ -1,7 +1,16 @@
 var http = require('http');
 
 var server = http.createServer( function (req, res) {
-  res.end("<html><body>Portal de notícias</body><html>");
+
+  var categoria = req.url;
+  
+  if (categoria === '/tecnologia') {
+    res.end("<html><body>Portal de tecnologia</body><html>");
+  } else if (categoria === '/moda') {
+    res.end("<html><body>Portal de moda</body><html>");
+  } else {
+    res.end("<html><body>Portal de notícias</body><html>");
+  }
 })
 
 server.listen(3000);
