3866436b65feef4ed47dfc0fd93ced3d8896d2a3
Antonio Lima Filho <antonio.filho@tvglobo.com.br>
2020-02-24 20:22:44 -0300

    refatorando aplicação

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.send("<html><body>Portal de noticias</body></html>")
+  res.render("home/index")
 });
 
-app.get('/tecnologia', function(req, res){
-  res.render("secao/tecnologia")
+app.get('/formulario_inclisao_noticia', function(req, res){
+  res.render("admin/form_add_noticia")
+});
+
+app.get('/noticias', function(req, res){
+  res.render("noticias/noticias")
 });
 
 app.listen(3000, function () { console.log("Servidor rodando com express") })
