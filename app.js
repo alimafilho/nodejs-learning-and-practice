e9f940f55723f52853b1e82f580feb2db32568eb
Antonio Lima Filho <antonio.filho@tvglobo.com.br>
2020-02-24 20:33:32 -0300

    implmentando modulos

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var msg = require('./mod_test');
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
@@ -15,4 +17,4 @@ app.get('/noticias', function(req, res){
   res.render("noticias/noticias")
 });
 
-app.listen(3000, function () { console.log("Servidor rodando com express") })
+app.listen(3000, function () { console.log("Servidor rodando com express", msg()) })
---
+++ mod_test.js
@@ -0,0 +1,5 @@
+
+module.exports = function () {
+  const msg = "Este modulo contém apenas uma string";
+  return msg;
+};
