48782927b5e047351b518b1fd79dd9f6d445bfc5
Antonio Lima Filho <antonio.filho@tvglobo.com.br>
2020-02-24 21:31:16 -0300

    implementação do mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,11 @@
+const dbconnection = require('../../config/dbconnection');
+
 module.exports= function (app) {
   app.get('/noticias', function(req, res){
-    res.render("noticias/noticias")
+    const conn = dbconnection();
+    const sql = 'select * from noticias';
+    conn.query(sql, function(err, result){
+      res.render("noticias/noticias", {noticias: result})
+    });
   });
 };
